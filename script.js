501e02c4b17d136bf06b4ad4afc086796e4ecc35
Andrew C Alvarez <31179195+AndrewCAlvarez@users.noreply.github.com>
2023-01-03 23:10:03 -0500

    Add animation to nav-list

--- script.js
+++ script.js
@@ -4,11 +4,13 @@ let isToggled = false;
 
 function toggleMenu() {
   if (isToggled) {
-    navList.style.display = "none";
+    // navList.style.display = "none";
+    navList.style.top = "-100vh";
     menuBtn.setAttribute("name", "menu-outline");
     isToggled = !isToggled;
   } else {
-    navList.style.display = "flex";
+    // navList.style.display = "flex";
+    navList.style.top = "0";
     menuBtn.setAttribute("name", "close-outline");
     isToggled = !isToggled;
   }
